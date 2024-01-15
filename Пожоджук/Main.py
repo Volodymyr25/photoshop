51cfb0ffc3480d546dbed95c8444ef4992050e72
Volodymyr25 <99071650+Volodymyr25@users.noreply.github.com>
2024-01-15 18:02:16 +0200

    Update Main.py

--- Пожоджук/Main.py
+++ Пожоджук/Main.py
@@ -6,3 +6,10 @@ with Image.open("Sobaka.jpg") as sobaka:
     print("Формат", sobaka.format)
     print("Тип", sobaka.mode)
     sobaka.show()
+
+    sobaka_gray = sobaka.convert("L")
+    sobaka_gray.save("gray.jpg")
+    print("Розмір", sobaka_gray.size)
+    print("Формат", sobaka_gray.format)
+    print("Тип", sobaka_gray.mode)
+    sobaka_gray.show()
