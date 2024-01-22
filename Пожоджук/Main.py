a525898b9e135f97a266289840540b398c3dfe84
Volodymyr25 <99071650+Volodymyr25@users.noreply.github.com>
2024-01-22 16:52:03 +0200

    Update Main.py

--- Пожоджук/Main.py
+++ Пожоджук/Main.py
@@ -7,9 +7,19 @@ with Image.open("Sobaka.jpg") as sobaka:
     print("Тип", sobaka.mode)
     sobaka.show()
 
+
     sobaka_gray = sobaka.convert("L")
     sobaka_gray.save("gray.jpg")
     print("Розмір", sobaka_gray.size)
     print("Формат", sobaka_gray.format)
     print("Тип", sobaka_gray.mode)
+
+    pic_blured = sobaka.filter(ImageFilter.BLUR)
+    pic_blured.save('blured.jpg')
+    pic_blured.show()
+
+    pic_up = sobaka.transpose(Image.ROTATE_180)
+    pic_up.save("up.jpg")
+    pic_up.show()
+
     sobaka_gray.show()
